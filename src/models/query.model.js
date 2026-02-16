1167739f6dc3ad76d3ff05f18c9bbc9288a9a474
Mayank <mayankkandpal2003@gmail.com>
2026-02-16 17:02:50 +0530

    Working on form

---
+++ src/models/query.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const querySchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    number:{
+        type:String,
+        required:true
+    },
+    query:{
+        type:String,
+        required:true
+    }
+})
+
+const Query = mongoose.model("Query", querySchema);
+export default querySchema;
